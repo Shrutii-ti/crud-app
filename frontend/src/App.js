29a18c203294e152b8726cdf518028368eecfe8c
AyushJHANWAR03 <ayushjhanwar12@gmail.com>
2025-05-30 12:03:08 +0530

    add: module import fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "../src/Componets/Navbar";
-import Home from "../src/Pages/Home";
-import Register from "../src/Pages/Register";
+import Home from "../src/pages/Home";
+import Register from "../src/pages/Register";
 
 function App() {
   return (
